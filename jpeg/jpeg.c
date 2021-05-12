d51c3f856a3f1f9ff275961b2870eed33c634f60
dbdexter-dev <dbdexter.dev@gmail.com>
2021-05-12 20:00:15 +0200

    Faster JPEG decoding (IDCT decomposition)

--- jpeg/jpeg.c
+++ jpeg/jpeg.c
@@ -1,11 +1,15 @@
+#include <string.h>
 #include "jpeg.h"
 #include "utils.h"
 #define Q_SHIFT 14
+#define Q_2 0x2000          /* 1/2, Q14 */
+#define Q_1SQRT2 0x2d41     /* 1/sqrt(2), Q14 */
 
 static void unzigzag(int16_t block[8][8]);
 static int quantization(int quality, int x, int y);
 static void dequantize(int16_t block[8][8], int quality);
 static void inverse_dct(uint8_t dst[8][8], int16_t src[8][8]);
+static int16_t qmul(int32_t x, int32_t y);
 
 /* Quantization table, standard 50% quality JPEG */
 static const uint8_t _quant[8][8] =
@@ -44,19 +48,6 @@ static const int16_t _cos_lut[8][8] =
 	{0x4000, (int16_t)0xdc72, (int16_t)0xe782, 0x3ec5, (int16_t)0xd2bf, (int16_t)0xf384, 0x3b21, (int16_t)0xcac9},
 	{0x4000, (int16_t)0xcac9, 0x187e, 0x0c7c, (int16_t)0xd2bf, 0x3ec5, (int16_t)0xc4df, 0x238e},
 	{0x4000, (int16_t)0xc13b, 0x3b21, (int16_t)0xcac9, 0x2d41, (int16_t)0xdc72, 0x187e, (int16_t)0xf384},
-};
-
-/* Alpha lookup table, Q14 */
-static const int16_t _alpha_lut[8][8] =
-{
-	{0x2000, 0x2d41, 0x2d41, 0x2d41, 0x2d41, 0x2d41, 0x2d41, 0x2d41},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
-	{0x2d41, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000},
 };
 
 void
@@ -108,25 +99,39 @@ static void
 inverse_dct(uint8_t dst[8][8], int16_t src[8][8])
 {
 	int i, j, u, v;
-	int32_t tmp;
+	int16_t alpha;
+	int32_t work[8][8];
+	int32_t row[8];
 
+	memset(work, 0, sizeof(work));
+
+	/* IDCT: cols */
 	for (i=0; i<8; i++) {
+		alpha = i ? 0x4000 : Q_1SQRT2;
 		for (j=0; j<8; j++) {
-			tmp = 0;
 			for (u=0; u<8; u++) {
-				for (v=0; v<8; v++) {
-					/* Happens quite often in JPEGs */
-					if (src[u][v] == 0) {
-						continue;
-					}
-					tmp += ((_alpha_lut[u][v]
-							* (((int32_t)_cos_lut[i][u] * (int32_t)_cos_lut[j][v]) >> Q_SHIFT)
-						   ) >> Q_SHIFT) * src[u][v];
-				}
+				work[j][u] += qmul(alpha, _cos_lut[u][i]) * src[j][i];
 			}
-			dst[i][j] = MAX(0, MIN(255, ((tmp/4 >> Q_SHIFT) + 128)));
 		}
 	}
+
+	/* IDCT: rows */
+	for (j=0; j<8; j++) {
+		memset(row, 0, sizeof(row));
+
+		for (i=0; i<8; i++) {
+			alpha = i ? 0x4000 : Q_1SQRT2;
+			for (v=0; v<8; v++) {
+				row[v] += ((int64_t)work[i][j] * qmul(alpha, _cos_lut[v][i])) >> Q_SHIFT;
+			}
+		}
+
+		/* Rescale from -512...512 to 0..256 */
+		for (i=0; i<8; i++) {
+			dst[i][j] = MAX(0, MIN(255, ((row[i]/4) >> Q_SHIFT) + 128));
+		}
+	}
+
 }
 
 static int
@@ -140,5 +145,12 @@ quantization(int quality, int x, int y)
 	}
 
 	return MAX(1, (((int)_quant[x][y] * compr_ratio / 50) + 1) / 2);
+}
+
+static int16_t
+qmul(int32_t x, int32_t y)
+{
+	const int32_t result = x * y;
+	return result >> Q_SHIFT;
 }
 /* }}} */
